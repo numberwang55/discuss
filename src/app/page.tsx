3cbda1032fc3b6e70623872c2213ae9b6e79081a
numberwang55 <cban0901@hotmail.com>
2024-07-02 15:43:27 +0100

    sign in & sign out buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
 import { Button } from "@nextui-org/react";
+import * as actions from "@/actions";
+import { auth } from "@/auth";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+
   return (
     <div>
-      <Button>Click me!</Button>
+      <form action={actions.signIn}>
+        <Button type="submit">Sign In</Button>
+      </form>
+      <form action={actions.signOut}>
+        <Button type="submit">Sign Out</Button>
+      </form>
+
+      {session?.user ? <div>Signed In</div> : <div>Signed Out</div>}
     </div>
   );
 }
