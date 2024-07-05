1d4b24aa240d29d9c699d8e9592a0ca74c8af895
numberwang55 <cban0901@hotmail.com>
2024-07-05 15:38:12 +0100

    header-auth client component - static route for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,7 @@
-import { Button } from "@nextui-org/react";
-import * as actions from "@/actions";
-import { auth } from "@/auth";
-import Profile from "@/components/profile";
-
-export default async function Home() {
-  const session = await auth();
-
-  return (
+export default function Home() {
+   return (
     <div>
-      <form action={actions.signIn}>
-        <Button type="submit">Sign In</Button>
-      </form>
-      <form action={actions.signOut}>
-        <Button type="submit">Sign Out</Button>
-      </form>
-
-      {session?.user ? <div>{JSON.stringify(session.user)}</div> : <div>Signed Out</div>}
-      <Profile></Profile>
+      Home Page
     </div>
   );
 }
