f331d303509f28e6b93d4c7e3a806d42f417dede
numberwang55 <cban0901@hotmail.com>
2024-07-24 12:17:39 +0100

    adding a loading skeleton to PostShowPage

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -5,6 +5,7 @@ import CommentCreateForm from "@/components/comments/comment-create-form";
 import paths from "@/paths";
 import { fetchCommentsByPostId } from "@/db/queries/comments";
 import { Suspense } from "react";
+import PostShowLoading from "@/components/posts/post-show-loading";
 
 interface PostShowPageProps {
   params: {
@@ -21,7 +22,7 @@ export default async function PostShowPage({ params }: PostShowPageProps) {
       <Link className="underline decoration-solid" href={paths.showTopic(slug)}>
         {"< "}Back to {slug}
       </Link>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<PostShowLoading/>}>
         <PostShow postId={postId} />
       </Suspense>
       <CommentCreateForm postId={postId} startOpen />
