3170b9c9e699583c1228c29d14ec035b4dde5f4b
numberwang55 <cban0901@hotmail.com>
2024-07-24 11:52:37 +0100

    Suspense react component used for content streaming

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -4,6 +4,7 @@ import CommentList from "@/components/comments/comment-list";
 import CommentCreateForm from "@/components/comments/comment-create-form";
 import paths from "@/paths";
 import { fetchCommentsByPostId } from "@/db/queries/comments";
+import { Suspense } from "react";
 
 interface PostShowPageProps {
   params: {
@@ -20,9 +21,11 @@ export default async function PostShowPage({ params }: PostShowPageProps) {
       <Link className="underline decoration-solid" href={paths.showTopic(slug)}>
         {"< "}Back to {slug}
       </Link>
-      <PostShow postId={postId}/>
+      <Suspense fallback={<div>Loading...</div>}>
+        <PostShow postId={postId} />
+      </Suspense>
       <CommentCreateForm postId={postId} startOpen />
-      <CommentList postId={postId}/>
+      <CommentList postId={postId} />
     </div>
   );
 }
