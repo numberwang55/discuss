7efb8548cc20925883520e1b51af56d350c56ebe
numberwang55 <cban0901@hotmail.com>
2024-07-24 11:36:48 +0100

    deduplicating requests with cache

--- src/db/queries/comments.ts
+++ src/db/queries/comments.ts
@@ -1,22 +1,25 @@
 import type { Comment } from "@prisma/client";
 import { db } from "..";
+import { cache } from "react";
 
 export type CommentWithAuthor = Comment & {
   user: { name: string | null; image: string | null };
 };
 
-export function fetchCommentsByPostId(postId: string): Promise<CommentWithAuthor[]> {
+export const fetchCommentsByPostId = cache(
+  (postId: string): Promise<CommentWithAuthor[]> => {
     console.log("making a query");
-    
+
     return db.comment.findMany({
-        where: { postId },
-        include: {
-            user: {
-                select: {
-                    name: true,
-                    image: true
-                }
-            }
-        }
-    })
-}
+      where: { postId },
+      include: {
+        user: {
+          select: {
+            name: true,
+            image: true,
+          },
+        },
+      },
+    });
+  }
+);
